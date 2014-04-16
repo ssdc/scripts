080b2fad1bdae089d49090b94d92aa897dd65713
ssdc <scottstevson@gmail.com>
2014-04-15 20:32:48 -0700

    adding my first repository of bash scripts

---
+++ chkargs2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+    then
+        echo "Usage: chkargs2 argument...." 1>&2
+        exit 1
+fi
+
+echo "Program running....."
+
+echo "Program complete."
+
+exit 0
---
+++ shebangAdder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this is pseudo code that explains 
+# what the shebangAdder script does
+
+# Steps:
+# 1 - read files as stdin
+# 2 - create two empty lines at the top of the file
+# 3 - add '#!/bin/bash' to the first line in each file
+# 4 - save each file
+# 5 - exit with code 0 or 1
+# 6 - if exit 1, then echo why that's so
+
+[ find . -type f ]
+    echo "found file"
